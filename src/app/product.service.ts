ed5982596205f246590fab85b5254c143065cd2c
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-11-15 21:42:24 +0100

    response to album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,10 +8,10 @@ import {Observable} from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  constructor(private _http: Http) {
+  constructor(private _http: Http) {}
 
-  }
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response): Album => response.json());
   }
+
 }
