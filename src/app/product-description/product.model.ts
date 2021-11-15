61ffa44e817cf2f7a2a2e7578dd69eb202542e8c
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-11-15 21:25:56 +0100

    models added

--- src/app/product-description/product.model.ts
+++ src/app/product-description/product.model.ts
@@ -1,19 +1,6 @@
 export interface ProductModel {
   id: number,
   artist: string,
-  album: Album
+  // album: Album
 }
 
-export interface Album {
-  name: string,
-  releaseDate: string,
-  coverImage: string,
-  tracks: Track[]
-}
-
-export interface Track {
-  trackNumber: number,
-  trackName: string,
-  trackLength: string,
-  trackPrice: number
-}
